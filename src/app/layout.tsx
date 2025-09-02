d35c0cb1b95cdcc2cabb4ca5a9c6ed0bb2deb356
davihtd <rodriguezever441@gmail.com>
2025-09-01 23:41:05 -0300

    src folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={geistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
+      <body suppressHydrationWarning className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
